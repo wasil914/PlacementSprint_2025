48dda8d8a64bf8ebf4f7dab510a7bf5091e40cf2
Wasil Khan <wasilkhan914@gmail.com>
2025-06-02 19:49:07 +0530

    How to print a string + newline (println ,\n)

---
+++ Week1_PrintHW.java
@@ -0,0 +1,19 @@
+class Week1_PrintHW{
+    public static void main(String[] args){
+        System.out.print("Wasil"); 
+        System.out.print("Khan\n");
+        // //output: WasilKhan
+
+         System.out.println("Wasil");
+         System.out.println("khan");
+        // // //output: Wasil Khan
+
+        System.out.print(105);
+        System.out.println(999);
+        System.out.print("Wasil\nKhan");
+        //output: 105999
+                //Wasil
+                //Khan
+
+    }
+}
