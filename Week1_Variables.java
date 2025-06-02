245aa6368d828cc22570a72cc74cf0dfe463c353
Wasil Khan <wasilkhan914@gmail.com>
2025-06-02 23:09:11 +0530

    Variables -1

---
+++ Week1_Variables.java
@@ -0,0 +1,44 @@
+class Week1_Variables {
+    public static void main(String[] args){
+
+        //Declare & Initialize a variable of type int 
+
+        int age;  //Declaration
+        age =23;  //Initialization
+
+        int Azhar_age=32;//Declaration & Initialization
+
+        System.out.println(age);
+        age= 88; //Prints latest initialization...
+        System.out.println(age);
+        System.out.println(Azhar_age);
+
+        int a=2,b=3,c=4;
+        System.out.println(a);
+                System.out.println(b);
+                        System.out.println(c);
+         //Variables : int , short , long l; float f ; double ; char ; boolean
+         
+        int Marks = 1000000000;
+         System.out.println(Marks);
+
+        short Smarks = 1000;
+        System.out.println(Smarks);
+
+        long Lmarks = 20000000000000l;
+        System.out.println(Lmarks);
+         
+        float num = 3.2345f;
+        double dec = 3.4565432345;
+        System.out.println(num + "\n" +dec );
+        
+        char S ='*';
+        System.out.println(S);
+
+        boolean B = true;
+        System.out.println(B);
+
+
+    }
+
+}
