dc7bbb926f30da732875248330419cbd133c8a2b
Wasil Khan <wasilkhan914@gmail.com>
2025-06-02 19:47:23 +0530

    Comments in Java

---
+++ Week1_Comments.java
@@ -0,0 +1,11 @@
+class Week1_Comments{
+    public static void main(String[] args){
+        // This is a single-line comment
+        /* Multi-line Comment
+         line 1 
+         line 2
+         line 3
+         and so on...
+         */
+    }
+}
