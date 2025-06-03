1b02c3deca6e0aa407eb7d6977a9afbdd372393b
Wasil Khan <wasilkhan914@gmail.com>
2025-06-04 00:08:24 +0530

    Sum of 1st N natural no.s

---
+++ Week1_P1Sum1toN.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Week1_P1Sum1toN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n*(n+1)/2);
+    }
+}
+
+/*
+long var1 = sc.nextLong();
+double var2 = sc.nextDouble();
+char ch = sc.nextChar();
+*/
