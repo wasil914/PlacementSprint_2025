060a1c5e9207c5adf3d43c8994df05056bda513b
Wasil Khan <wasilkhan914@gmail.com>
2025-06-03 19:27:02 +0530

    Type Conversion (e.g : int , long)

---
+++ Week1_TypeConversion.java
@@ -0,0 +1,10 @@
+class Week1_Typeconversion{
+    public static void main(String[] args){
+        int A = 100;
+        long B = 10000l;
+
+        A = (int)B;
+        System.out.println(B);
+        
+    }
+}
