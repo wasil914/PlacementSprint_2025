3e575663eb072dbbcef698aa9ac58abbf57579c1
Wasil Khan <wasilkhan914@gmail.com>
2025-06-04 00:07:44 +0530

    User Input in Java

---
+++ Week1_UserInput.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Week1_UserInput {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt(); 
+        System.out.println("You typed : " + n);
+    }
+    
+}
