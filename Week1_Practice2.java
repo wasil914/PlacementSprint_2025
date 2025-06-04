c62f29e0cdbb59de883c4c464ee283a54bf2c7ed
Wasil Khan <wasilkhan914@gmail.com>
2025-06-04 18:58:59 +0530

    Week 1 Practice 2

---
+++ Week1_Practice2.java
@@ -0,0 +1,9 @@
+public class Week1_Practice2 {
+    public static void main(String[] args){
+        int i =32;
+        
+        System.out.println(~i);
+                System.out.println(i);
+
+    }
+}
