ed11709bd2b5c22e744831549f29a39b6f0afbec
Wasil Khan <wasilkhan914@gmail.com>
2025-06-04 18:59:29 +0530

    Week 1 Some extra concepts

---
+++ Concepts.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+
+//Sort an Array
+// class Concepts{
+//     public static void main(String [] args){
+//         int [] numbers = {39,47,31,14,15,99};
+//         Arrays.sort(numbers);
+//         System.out.println(Arrays.toString(numbers));
+//     };
+
+// }
+
+class Concepts{
+    public static void main(String []  args){
+        // Scanner userinput = new Scanner (System.in);
+        // int n = userinput.nextInt();
+        // System.out.println("Typed this integer "+ n);
+
+        
+    }
+}
