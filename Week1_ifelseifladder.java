7ee9c4789a35701f6408fbb787f22081db86123e
Wasil Khan <wasilkhan914@gmail.com>
2025-06-08 15:37:46 +0530

    if-elseif-else ladder

---
+++ Week1_ifelseifladder.java
@@ -0,0 +1,18 @@
+ class Week1_ifelseifladder {
+    public static void main(String[] args){
+    int marks = 68;
+
+    if(marks >=80){
+    System.out.println("A+");
+    }
+    else if(marks>=60){
+    System.out.println("B+");
+    }
+    else if(marks>=40){
+        System.out.println("C+");
+    }
+    else{
+        System.out.println("Fail");
+    }
+ }
+}
