7ab38342926c83fe6c26a16a310b12231229690e
Wasil Khan <wasilkhan914@gmail.com>
2025-06-10 22:53:28 +0530

    Week1_tables

---
+++ Week1_tables.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Week1_tables {
+    public static void main(String[] args){
+        System.out.println("Enter N : ");
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+
+        for(int i=1;i<=10;i+=1){
+            System.out.println(n*i);
+
+        }
+    }
+}
