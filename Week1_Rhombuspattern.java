06addc806db5881623011d0de2fac2ad1f90c6dc
Wasil Khan <wasilkhan914@gmail.com>
2025-06-14 22:12:33 +0530

    Week1_Rhombuspattern

---
+++ Week1_Rhombuspattern.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Week1_Rhombuspattern{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int N = sc.nextInt();
+
+       for(int o =0;o<N;o++){
+        
+       }
+    }
+}
