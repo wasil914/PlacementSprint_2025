6772b225cabee19eb12a5d82b3cbd7ac646a9c50
Wasil Khan <wasilkhan914@gmail.com>
2025-06-20 23:17:06 +0530

    arrweek3

---
+++ Week3_array10.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Week3_array10 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int N = sc.nextInt() ;
+        int arr[] = new int[N];
+        for(int i=0;i<N;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+    
+}
