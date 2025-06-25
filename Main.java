e3c1b03f53f0f3023b894f722fea9b495d97062a
Wasil Khan <wasilkhan914@gmail.com>
2025-06-25 22:31:05 +0530

    fact !

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();
+        long fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact *= i;
+        }
+        System.out.println(fact);
+    }
+}
