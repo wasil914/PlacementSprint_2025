f78fd10e521b4d035f51c012f004d1e87cd32cc2
Wasil Khan <wasilkhan914@gmail.com>
2025-06-27 23:21:00 +0530

    Add Two Numbers (Linked Lists)

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
+
 public class Main {
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
         int n = sc.nextInt();
-        long fact = 1;
-        for (int i = 2; i <= n; i++) {
-            fact *= i;
+        boolean isPrime = true;
+        if (n <= 1) isPrime = false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
         }
-        System.out.println(fact);
+        System.out.println(isPrime ? "YES" : "NO");
     }
 }
---
+++ lc201.java
@@ -0,0 +1,30 @@
+//Add Two Numbers (Linked Lists)
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if (carry != 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
+}
