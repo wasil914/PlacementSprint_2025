2c2960e7b5192271e789ae1fb4eef8ac855f514a
Wasil Khan <wasilkhan914@gmail.com>
2025-06-27 23:21:55 +0530

    Container With Most Water

---
+++ lc204.java
@@ -0,0 +1,14 @@
+//Container With Most Water
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+        return maxArea;
+    }
+}
